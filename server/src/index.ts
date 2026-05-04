a711da7f37efea8c295a5269d1feea4b8627b179
Romain Debas <debasromain@gmail.com>
2026-05-04 16:03:12 +0200

    adding the last version of the resume, modify the head in index.html for Open Graph for the link of the portfolio, and put back the right routes for the local use

--- server/src/index.ts
+++ server/src/index.ts
@@ -79,10 +79,10 @@ app.post(
 		}
 	},
 );
-// const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5001;
 
-// // app.listen(PORT, () => {
-// // 	console.log(`Serveur prêt sur le port ${PORT}`);
-// // });
+app.listen(Number(PORT), "0.0.0.0", () => {
+	console.log(`Serveur prêt sur le port ${PORT}`);
+});
 
 export default app;
